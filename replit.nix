3202effa9a52d08258667e317c8af34ac4e63d6b
savansajith <savansajith2002@gmail.com>
2024-06-17 11:09:22 +0000

    #container stuff

---
+++ replit.nix
@@ -0,0 +1,15 @@
+{pkgs}: {
+  deps = [
+    pkgs.libcxx
+    pkgs.tk
+    pkgs.tcl
+    pkgs.qhull
+    pkgs.pkg-config
+    pkgs.gtk3
+    pkgs.gobject-introspection
+    pkgs.ghostscript
+    pkgs.freetype
+    pkgs.ffmpeg-full
+    pkgs.cairo
+  ];
+}
