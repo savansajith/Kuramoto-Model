ffa10b4b1942514fe69debddae67276fb000f655
savansajith <savansajith2002@gmail.com>
2024-06-18 10:22:53 +0000

    Added decriptions
    Added decriptions by Separating the eqs

--- app.py
+++ app.py
@@ -3,29 +3,39 @@ from sympy import *
 
 # Define symbols
 i, j, k, K, N, t = symbols('i j k K N t')
-K1 = Symbol("K1")
-K2 = Symbol("K2")
+K_1 = Symbol("K_1")
+K_2 = Symbol("K_2")
 theta_i = Function("theta_i")
 theta_j = Function("theta_j")
 theta_k = Function("theta_k")
 omega_i = Symbol("omega_i")
 
 # Define equations
-expr1 = Eq(diff(theta_i(t), t), omega_i + (K / N) * Sum(sin(theta_j(t) - theta_i(t)), (j, 1, N)))
-expr2 = Eq(diff(theta_i(t), t), omega_i + (K1 / N) * Sum(sin(-theta_i(t) + theta_j(t)), (i, 1, N)) + (K2 / N**2) * Sum(Sum(sin(2 * theta_j(t) - theta_k(t) - theta_i(t)), (j, 1, N)), (k, 1, N)))
+expr1_order = Eq(diff(theta_i(t), t), omega_i + (K / N) * Sum(sin(theta_j(t) - theta_i(t)), (j, 1, N)))
+expr2_order = Eq(diff(theta_i(t), t), omega_i + (K_1 / N) * Sum(sin(-theta_i(t) + theta_j(t)), (i, 1, N)) + (K_2 / N**2) * Sum(Sum(sin(2 * theta_j(t) - theta_k(t) - theta_i(t)), (j, 1, N)), (k, 1, N)))
 
 # Convert equations to LaTeX format
-latex_expr1 = latex(expr1)
-latex_expr2 = latex(expr2)
+latex_eq1 = latex(expr1_order)
+latex_eq2 = latex(expr2_order)
+
+latex_theta_i = latex(theta_i)
+latex_theta_j = latex(theta_j)
+latex_theta_k = latex(theta_k)
+latex_omega_i = latex(omega_i)
+latex_K = latex(K)
+latex_K1 = latex(K_1)
+latex_K2 = latex(K_2)
+latex_N = latex(N)
+
 
 Eqs_lst = [
     {
-             "order": "First Order:",
-             "eq": latex_expr1
+             "order": "First Order :",
+             "eq": latex_eq1
     },
     {
-             "order": "Second Order:",
-             "eq": latex_expr2
+             "order": "Second Order :",
+             "eq": latex_eq2
     }
 ]
 
